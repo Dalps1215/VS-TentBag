d639149c66e0e8a538a3d426a3923ea22d415ede
Drew <dalps1215@gmail.com>
2025-01-24 16:44:37 -0500

    Update Config.cs

--- src/configuration/Config.cs
+++ src/configuration/Config.cs
@@ -4,9 +4,9 @@ namespace tentbag.configuration;
 
 [SuppressMessage("ReSharper", "AutoPropertyCanBeMadeGetOnly.Global")]
 public class Config {
-    public int Radius { get; set; } = 3;
-    public int Height { get; set; } = 7;
-    public float BuildEffort { get; set; } = 100F;
+    public int Radius { get; set; } = 5;
+    public int Height { get; set; } = 8;
+    public float BuildEffort { get; set; } = 0F;
     public bool RequireFloor { get; set; } = false;
     public bool ReplacePlantsAndRocks { get; set; } = true;
     public bool PutTentInInventoryOnUse { get; set; } = true;
